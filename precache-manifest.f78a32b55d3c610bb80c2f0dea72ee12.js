4e9c34f9af9a88e99d71fad2ffda17fc0170e98c
Nevena Kostic <nevena@MacBook-Pro-3.local>
2020-05-24 23:47:27 +0200

    Updates

---
+++ precache-manifest.f78a32b55d3c610bb80c2f0dea72ee12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecdb3a5d5f3654dbbbefcb30891a1621",
+    "url": "/bubamarica1/movie.git/index.html"
+  },
+  {
+    "revision": "32275ea71af35a209d7b",
+    "url": "/bubamarica1/movie.git/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "5dc77d5203af50160f69",
+    "url": "/bubamarica1/movie.git/static/js/2.9ebd5223.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bubamarica1/movie.git/static/js/2.9ebd5223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32275ea71af35a209d7b",
+    "url": "/bubamarica1/movie.git/static/js/main.a8b8ddd8.chunk.js"
+  },
+  {
+    "revision": "166059efeb39374bdb39",
+    "url": "/bubamarica1/movie.git/static/js/runtime-main.2b54fc54.js"
+  }
+]);
